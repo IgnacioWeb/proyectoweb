98ff9e20448b1ad4c242f0fdd1a7f65a2a75f177
IgnacioWeb <nacho.aoi16@gmail.com>
2020-12-22 17:25:01 -0300

    Creación de la boleta/imprimir

--- ventas2/__manifest__.py
+++ ventas2/__manifest__.py
@@ -24,7 +24,8 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/view_datos.xml',
+        'views/view_datos.xml', 
+        'views/boleta.xml',
         #'views/templates.xml',
     ],
 }
--- ventas2/models/tipodatos.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class TipoDato(models.Model):
-    _name = 'ventas2.datos2'
-
-    name = fields.Char(string="Nombre", required=True)
